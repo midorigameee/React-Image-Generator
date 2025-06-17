d0b8cdb1988e615a25c0f0a92554baeaa986b5b6
midorigameee <xqbodp727@gmail.com>
2025-06-17 13:28:45 +0000

    画像選択フォームのロジックをリファクタリングします

--- frontend/src/features/imageUploader/components/FileSelectForm.tsx
+++ frontend/src/features/imageUploader/components/FileSelectForm.tsx
@@ -1,17 +1,45 @@
 import React from "react";
 import "./FileSelectForm.css";
+import { extractExifDataFromFile } from "../imageUtils";
+import type { ExifData } from "../types";
 
 type Props = {
-  handleFileChange: (
-    event: React.ChangeEvent<HTMLInputElement>
-  ) => Promise<void>;
-  /* 解説
-    onChangeイベントの時は、Reactは event: React.ChangeEvent<HTMLInputElement> を
-    暗黙的にpropsに渡しているため、引数として定義する必要がある。
-   */
+  setSelectedFile: React.Dispatch<React.SetStateAction<File | null>>;
+  setProcessedImage: React.Dispatch<React.SetStateAction<string | null>>;
+  setStatusMessage: React.Dispatch<React.SetStateAction<string | null>>;
+  setExifData: React.Dispatch<React.SetStateAction<ExifData | null>>;
 };
 
-const FileSelectForm: React.FC<Props> = ({ handleFileChange }) => {
+const FileSelectForm: React.FC<Props> = ({
+  setSelectedFile,
+  setProcessedImage,
+  setStatusMessage,
+  setExifData,
+}) => {
+  const handleFileChange = async (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    if (event.target.files && event.target.files.length > 0) {
+      const file = event.target.files[0];
+
+      // JPEG かどうかをチェック
+      if (file.type !== "image/jpeg" && file.type !== "image/jpg") {
+        setStatusMessage("JPEG画像（.jpg / .jpeg）のみアップロードできます。");
+        setSelectedFile(null);
+        return;
+      }
+
+      const exif = await extractExifDataFromFile(file);
+      if (exif) {
+        setExifData(exif);
+      }
+
+      setSelectedFile(file);
+      setProcessedImage(null); // 新しくファイルを選択したら過去の画像はリセット
+      setStatusMessage(""); // ステータスメッセージもリセット
+    }
+  };
+
   return (
     <div className="file-input-form">
       <label className="custom-file-upload">
--- frontend/src/features/imageUploader/components/ImageUploader.tsx
+++ frontend/src/features/imageUploader/components/ImageUploader.tsx
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
 import "./ImageUploader.css";
-import { extractExifDataFromFile } from "../imageUtils";
 import ToggleSwitch from "../../../components/ToggleSwitch";
 import UploadButton from "./UploadButton";
 import FileSelectForm from "./FileSelectForm";
 import ShowExifIInfo from "./ShowExifIInfo";
+import type { ExifData } from "../types";
 
 type Props = {
   setStatusMessage: React.Dispatch<React.SetStateAction<string | null>>;
@@ -17,38 +17,19 @@ const ImageUploader: React.FC<Props> = ({
 }) => {
   const [selectedFile, setSelectedFile] = useState<File | null>(null);
   const [showExif, setShowExif] = useState<boolean>(true);
-  const [exifData, setExifData] = useState<any>(null);
+  const [exifData, setExifData] = useState<ExifData | null>(null);
 
   console.log(exifData);
-
-  const handleFileChange = async (
-    event: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    if (event.target.files && event.target.files.length > 0) {
-      const file = event.target.files[0];
-
-      // JPEG かどうかをチェック
-      if (file.type !== "image/jpeg" && file.type !== "image/jpg") {
-        setStatusMessage("JPEG画像（.jpg / .jpeg）のみアップロードできます。");
-        setSelectedFile(null);
-        return;
-      }
-
-      const exif = await extractExifDataFromFile(file);
-      if (exif) {
-        setExifData(exif);
-      }
-
-      setSelectedFile(file);
-      setProcessedImage(null); // 新しくファイルを選択したら過去の画像はリセット
-      setStatusMessage(""); // ステータスメッセージもリセット
-    }
-  };
 
   return (
     <div className="uploader-container">
       <div className="form-item">
-        <FileSelectForm handleFileChange={handleFileChange} />
+        <FileSelectForm
+          setSelectedFile={setSelectedFile}
+          setProcessedImage={setProcessedImage}
+          setStatusMessage={setStatusMessage}
+          setExifData={setExifData}
+        />
         <UploadButton
           selectedFile={selectedFile}
           showExif={showExif}
